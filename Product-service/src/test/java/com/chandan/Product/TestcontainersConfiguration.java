e677f6beebf1d7391edb5b9574603830650d97a5
Chandan Kumar <shwetachanndu1994@gmail.com>
2025-02-23 23:24:01 +0530

    first commit

---
+++ Product-service/src/test/java/com/chandan/Product/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.chandan.Product;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
